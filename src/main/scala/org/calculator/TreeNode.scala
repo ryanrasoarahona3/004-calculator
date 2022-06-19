6a15b3660e9af7d132a39724ca71f036fe73326b
ryanrasoarahona3 <ryanrasoarahona3@gmail.com>
2022-06-20 00:05:57 +0300

    Ajout du premier cas de test

--- src/main/scala/org/calculator/TreeNode.scala
+++ src/main/scala/org/calculator/TreeNode.scala
@@ -1,9 +1,44 @@
 package org.calculator
 
-class TreeNode(expression: String) {
-  val left: TreeNode = null
+case class TreeNode(expression: String) {
+  var left: TreeNode = null
   var right: TreeNode = null
-  var content: Any = null
+  var content: Float = 0.0f
+
+  /**
+   * Permet de vérifier si l'expression est un simple nombre (à ne plus décomposer)
+   * Sinon, on le décompose
+   *
+   * TODO: Trouver un moyen de simplifier le code : comme un héritage de classe ou autres..
+   * @return
+   */
+  private def isASimpleNumber(): Boolean = {
+    try {
+      expression.toFloat
+    } catch {
+      case e: NumberFormatException => return false
+    }
+    true
+  }
+
+  /**
+   * Cette méthode permet de calculer la valeur saisie
+   * @return
+   */
+  def evaluate(): Float = {
+    if(left == null && right == null){
+      return content.asInstanceOf[Float]
+    }
+    throw new Exception("Syntaxe non prise en compte")
+  }
+
+  // l'algorithme est ici
+  // On utilisera un algorithme récursif
+  if(isASimpleNumber()) { // Le cas le plus simple
+    left = null
+    right = null
+    content = expression.toFloat
+  }
 
   print("Hello world, this is the constructor")
 }
--- src/test/scala/TestTreeNode.scala
+++ src/test/scala/TestTreeNode.scala
@@ -1,7 +1,12 @@
 
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.calculator.TreeNode
 
 class TestTreeNode extends AnyFunSuite {
   // Tests are here
+  test("TreeNode.simpleNumber") {
+    val p = new TreeNode("3")
+    assert(p.evaluate() == 3)
+  }
 }
