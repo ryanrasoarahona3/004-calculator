cb6f6454c10624be416718176e8635cff73d2a53
ryanrasoarahona3 <ryanrasoarahona3@gmail.com>
2022-06-20 21:10:45 +0300

    Les lignes sont là

--- src/main/scala/org/calculator/Canvas.scala
+++ src/main/scala/org/calculator/Canvas.scala
@@ -2,6 +2,7 @@ package org.calculator
 
 import java.awt.{Color, Graphics2D}
 import scala.swing.{Dimension, Panel}
+import scala.util.control.Breaks.break
 
 class Canvas extends Panel {
   override def paintComponent(g: Graphics2D): Unit ={
@@ -10,11 +11,19 @@ class Canvas extends Panel {
     g.setColor(Color.blue)
     g.fillOval(0, 0, 100, 100)
 
+
     // Tracer les grilles
     // Doit trouver un système de test pour automatiser la création des grilles
     // Une matrice de transforamtion devrait aussi être utilisé pour la conversion des coordonnées
     g.setColor(Color.gray)
     g.drawLine(200, 0, 200, 400)
+    val t = new Transform(200, 200, 80)
+    // Vertical Right Dark Gray
+    for(x <- 0 to 4){
+      var _x = t.apply(x, 0)._1
+      g.drawLine(_x.toInt, 0, _x.toInt, 400)
+      print("Hi")
+    }
   }
 
   this.preferredSize = new Dimension(400, 400);
