a85aa99db141d231eeea21f89b05bef3082759e1
ryanrasoarahona3 <ryanrasoarahona3@gmail.com>
2022-06-22 09:31:10 +0300

    Code à moitié

--- src/main/scala/org/calculator/TreeNode2.scala
+++ src/main/scala/org/calculator/TreeNode2.scala
@@ -36,4 +36,29 @@ class TreeNode2(expression: String, maskContent: String = "") {
     val pattern = "_+".r
     pattern.matches(expression)
   }
+
+  def evaluate(xVar:Float = 0.0f): Float = {
+    if(left == null && right == null){
+      if(content == "x"){
+        return xVar
+      }else {
+        return content.asInstanceOf[Float]
+      }
+    }else{
+      return 0.0f
+    }
+  }
+
+  def init(): Unit ={
+    if(isASimpleNumber()) { // Le cas le plus simple
+      left = null
+      right = null
+      content = expression.toFloat
+    }else if(isASimpleVariable()){
+      left = null
+      right = null
+      content = "x"
+    }
+  }
+  init()
 }
--- src/test/scala/TestTreeNode2.scala
+++ src/test/scala/TestTreeNode2.scala
@@ -3,8 +3,19 @@ import org.calculator.TreeNode2
 import org.scalatest.funsuite.AnyFunSuite
 
 class TestTreeNode2 extends AnyFunSuite {
-  test("TreeNode.simpleNumber") {
+
+  test("TreeNode2.simpleNumber") {
     val p = new TreeNode2("3")
     assert(p.evaluate() == 3)
+  }
+
+  test("TreeNode2.simpleVariable") {
+    val p = new TreeNode2("x")
+    assert(p.evaluate(2.0f) == 2.0f)
+  }
+
+  test("TreeNode2.simplePlaceholder") {
+    val p = new TreeNode2("_", "5.0");
+    assert(p.evaluate() == 0.5f)
   }
 }
