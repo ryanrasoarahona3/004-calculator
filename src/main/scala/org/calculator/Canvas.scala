3be7b5cd6700c1c0558021ed304c5126bd7d7edc
ryan <ryanrasoarahona3@gmail.com>
2022-06-25 15:42:04 +0300

    Add antialiasing

--- src/main/scala/org/calculator/Canvas.scala
+++ src/main/scala/org/calculator/Canvas.scala
@@ -1,14 +1,20 @@
 package org.calculator
 
-import java.awt.{BasicStroke, Color, Graphics2D, Stroke}
+import java.awt.{BasicStroke, Color, Graphics2D, RenderingHints, Stroke}
+import java.util
 import scala.swing.{Dimension, Panel}
-import scala.util.control.Breaks.break
+import scala.jdk.CollectionConverters._
+
 
 class Canvas extends Panel {
   var p: TreeNode2 = TreeNode2("0")
+  var q: TreeNode2 = TreeNode2("0")
 
   override def paintComponent(g: Graphics2D): Unit ={
     var t = new Transform(200, 200, 80) // Should be an element property
+    //g.setRenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+
+    g.setRenderingHints(Map(RenderingHints.KEY_ANTIALIASING -> RenderingHints.VALUE_ANTIALIAS_ON).asJava)
 
     g.clearRect(0, 0, size.width, size.height)
     /*
@@ -42,10 +48,24 @@ class Canvas extends Panel {
 
     // Draw a f(x)=x function
     val o = p.evaluateWithinInterval(-4.0f, 4.0f, 0.01f)
+    val r = q.evaluateWithinInterval(-4.0f, 4.0f, 0.01f)
 
     g.setPaint(Color.BLUE)
+    g.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f))
     var prev_point = o(0)
     for(point <- o){
+      if(prev_point != point){
+        val p1 = t.apply(prev_point._1, prev_point._2)
+        val p2 = t.apply(point._1, point._2)
+        g.drawLine(p1._1.toInt, p1._2.toInt, p2._1.toInt, p2._2.toInt)
+      }
+      prev_point = point
+    }
+
+    g.setPaint(Color.GREEN)
+    g.setStroke(new BasicStroke(1.0f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f))
+    prev_point = r(0)
+    for(point <- r){
       if(prev_point != point){
         val p1 = t.apply(prev_point._1, prev_point._2)
         val p2 = t.apply(point._1, point._2)
--- src/main/scala/org/calculator/Main.scala
+++ src/main/scala/org/calculator/Main.scala
@@ -22,12 +22,16 @@ object Main {
       }
       formulaInput.border = BorderFactory.createEmptyBorder(5,5,2,5)
 
+      val derivationLabel = new Label("f'(x)=")
       val derivationField = new TextField()
       val derivationOutput = new BorderPanel {
-        add(new Label("f'(x)="), BorderPanel.Position.West)
+        add(derivationLabel, BorderPanel.Position.West)
         add(derivationField, BorderPanel.Position.Center)
       }
       derivationOutput.border = BorderFactory.createEmptyBorder(0,5,5,5)
+      derivationLabel.foreground = new Color(0, 140, 0)
+      derivationField.foreground = new Color(0, 140, 0)
+
 
       var bottomControls = new BoxPanel(Orientation.Vertical) {
         contents+= formulaInput
@@ -41,7 +45,9 @@ object Main {
       reactions+= {
         case ButtonClicked(`goButton`) => {
           val p = new TreeNode2(formulaField.text)
+          val q = p.derivate()
           canvas.p = p
+          canvas.q = q
           canvas.repaint()
 
           val d = p.derivate().getSimplified().getSimplified()
